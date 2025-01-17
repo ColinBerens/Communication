fe8417a3e14f72c53eba6a4a64488c3d73dc55f3
DerCoolin1 <colin.berens@student.howest.be>
2025-01-17 04:06:02 +0100

    Navbar

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const aboutMeButton = document.getElementById('aboutMeButton');
+
+// Add a click event listener to the button
+aboutMeButton.addEventListener('click', function() {
+  // Redirect to work.html when the button is clicked
+  window.location.href = 'aboutMe.html';
+});
