a9784ff027749ff680936027b1dc9d8b372b76e8
Colin Berens <colin.berens@student.howest.be>
2025-10-17 23:33:32 +0200

    Update Portfolio fo new CV and Removal of obsolet files

--- PHP/sendEmail.php
+++ PHP/sendEmail.php
@@ -1,22 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Process the form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = "colin.berens@hotmail.com";  // Replace with your email address
+    $to = "colin.berens@hotmail.com"; // your receiving address
     $subject = "New Message from $name";
     $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-    $headers = "From: $email";
+
+    // Use a domain-based 'From' address
+    $headers = "From: website@yourdomain.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($to, $subject, $body, $headers)) {
-        // Redirect back to the main page with a success message
         header('Location: ../index.html?success=1');
-        
         exit;
     } else {
-        // Redirect back to the main page with an error message
         header('Location: ../index.html?success=0');
         exit;
     }
